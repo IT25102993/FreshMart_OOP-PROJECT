5eaa1ae000833840f14697f3405ac2084bc576d4
IT25102993 <it25102993@my.sliit.lk>
2026-05-18 19:52:22 +0530

    Delete PremiumCustomer.java

--- src/main/java/com/freshmart/entity/PremiumCustomer.java
+++
@@ -1,9 +0,0 @@
-package com.freshmart.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.DiscriminatorValue;
-
-@Entity
-@DiscriminatorValue("PREMIUM")
-public class PremiumCustomer extends Customer {
-}
